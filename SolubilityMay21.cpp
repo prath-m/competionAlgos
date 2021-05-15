71179bfc71ba9254305f3cc3ca442bbb5d583151
Pratham Sharma <thegreatpratham18@gmail.com>
2021-05-15 23:23:54 +0530

    Create SolubilityMay21.cpp

---
+++ SolubilityMay21.cpp
@@ -0,0 +1,41 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input = new Scanner(System.in);
+		double X, A, B;
+		int T;
+		
+		T = input.nextInt();
+		
+	   for(int i=0; i<T; i++){
+	      X = input.nextInt();
+		   A = input.nextInt();
+		   B = input.nextInt();
+		   
+		   while(X != 100){
+		      A += B;
+		      X++;
+		   }
+		   A *= 10;
+		   System.out.printf("%.0f", A);
+		   System.out.print("\n");
+	   }
+	   
+	}
+}
+
+/*
+Inputs:
+X - Temperature
+A - Solutbility
+B - Increase of solubility
+if(X++) == A+B
+*/
